938ac2698e68aaecde970e3ca4f7517f99ff15d2
Josh Katz <josh@xqmsg.com>
2023-12-04 23:56:10 -0500

    add build

---
+++ build/CMakeFiles/imageproc.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/imageproc.dir/src/Image.cpp.o"
+  "CMakeFiles/imageproc.dir/src/Image.cpp.o.d"
+  "CMakeFiles/imageproc.dir/src/main.cpp.o"
+  "CMakeFiles/imageproc.dir/src/main.cpp.o.d"
+  "imageproc"
+  "imageproc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/imageproc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
